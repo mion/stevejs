8d11d7ff647edac1c5ddd3297a7b28357d19ba1a
Gabriel Vieira <gluisvieira@gmail.com>
2013-10-31 23:45:39 -0200

    Add license and shit.

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import json
 from search_engine import SearchEngine
 from flask import Flask, request
---
+++ steve.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Steve
+~~~~~
+
+This is not the text editor you're looking for.
+
+:copyright: (c) 2013 by Gabriel Vieira.
+:license: MIT, see LICENSE for more details.
+"""
